356b6bdc20b568d94055abd11ffcc0a77b13d896
Natália Diotto <diottonatalia@gmail.com>
2023-12-10 11:43:18 -0300

    Lista 03 - Exercicio 05

--- src/Lista03/Exercicio05.java
+++ src/Lista03/Exercicio05.java
@@ -1,6 +1,29 @@
 package Lista03;
 
+import java.util.Locale;
+
 public class Exercicio05 {
+
+    public static void calcularCombustivel(double tempo, double velocidade) {
+        if (velocidade < 0 || tempo < 0) {
+            System.out.println("A velocidade média e o tempo não podem ser negativos.");
+
+        }
+
+        double consumoPorLitro = 12.0;
+        double distancia = tempo * velocidade;
+        double totalCombustivel = distancia / consumoPorLitro;
+        System.out.printf("%n Para percorrer %.2fkm, serão necessários %.3f litros de combustível.",
+                distancia, totalCombustivel);
+    }
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        calcularCombustivel(2.5, 80.0);
+        calcularCombustivel(3, 100.0);
+
+    }
 
 
 }
